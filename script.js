a78d236b940a3d2a59c3aacf0054ae518604c496
Vignesh Raj R <52847146+vicky-vr@users.noreply.github.com>
2020-09-10 13:25:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function navslide(){
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        burger.classList.toggle('toggle');
+    })
+}
+
+navslide();
